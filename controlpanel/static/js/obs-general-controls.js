793471945498892d83e72b52775245c88ed50637
rafaelurben <github@rafaelurben.ch>
2023-05-27 15:31:00 +0200

    Added more record/stream controls+data

--- controlpanel/static/js/obs-general-controls.js
+++ controlpanel/static/js/obs-general-controls.js
@@ -19,21 +19,49 @@ async function loadScenes() {
 }
 
 async function loadStatus() {
-    let recordStatusData = await sendOBSCommand("GetRecordStatus");
-    let streamStatusData = await sendOBSCommand("GetStreamStatus");
-
+    let recDat = await sendOBSCommand("GetRecordStatus");
+    
     let recordStatusDisplay = $("#obs-record-status-display");
-    let streamStatusDisplay = $("#obs-stream-status-display");
+    let recordStartBtn = $("#obs-record-start-btn");
+    let recordStopBtn = $("#obs-record-stop-btn");
 
-    if (recordStatusData.outputActive) {
-        recordStatusDisplay.val(`Aufnahme ${recordStatusData.outputTimecode.split(".")[0]}`);
+    if (recDat.outputActive) {
+        recordStartBtn.toggleClass("d-none", true);
+        recordStopBtn.toggleClass("d-none", false);
+        
+        let recTsp = recDat.outputTimecode.split(".")[0];
+        let recMB = (recDat.outputBytes / 1024 / 1024).toFixed(3);
+        var txt = `${recTsp} (${recMB} MB)`;
+        recordStatusDisplay.val(txt);
     } else {
         recordStatusDisplay.val("Keine Aufnahme");
+        recordStartBtn.toggleClass("d-none", false);
+        recordStopBtn.toggleClass("d-none", true);
     }
 
-    if (streamStatusData.outputActive) {
-        streamStatusDisplay.val(`Stream ${streamStatusData.outputTimecode.split(".")[0]} (Skipped: ${streamStatusData.outputSkippedFrames})`);
+    let strDat = await sendOBSCommand("GetStreamStatus");
+
+    let streamStatusDisplay = $("#obs-stream-status-display");
+    let streamStartBtn = $("#obs-stream-start-btn");
+    let streamStopBtn = $("#obs-stream-stop-btn");
+
+    if (strDat.outputActive) {
+        streamStartBtn.toggleClass("d-none", true);
+        streamStopBtn.toggleClass("d-none", false);
+
+        let strTsp = strDat.outputTimecode.split(".")[0];
+        let strMB = (strDat.outputBytes / 1024 / 1024).toFixed(3);
+        var txt = `${strTsp} (${strMB} MB, Skipped: ${strDat.outputSkippedFrames} / ${strDat.outputTotalFrames})`;
+        if (strDat.outputReconnecting) {
+            txt += " Reconnecting...";
+        }
+        if (strDat.outputCongestion) {
+            txt += " Congestion: " + strDat.outputCongestion;
+        }
+        streamStatusDisplay.val(txt);
     } else {
+        streamStartBtn.toggleClass("d-none", false);
+        streamStopBtn.toggleClass("d-none", true);
         streamStatusDisplay.val("Kein Stream");
     }
 }
