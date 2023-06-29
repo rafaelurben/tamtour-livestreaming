fcc7c90930a00f150ab6ffb7932f592cbab79e4b
rafaelurben <github@rafaelurben.ch>
2023-06-29 17:18:47 +0200

    Added color selectors

--- controlpanel/static/js/atem-diverses.js
+++ controlpanel/static/js/atem-diverses.js
@@ -2,6 +2,46 @@
 
 let atemMP1selectElem = $("#atem-mp1-source-select")
 
+// Utils
+
+function hslToHex(h, s, l) {
+    // Convert HSL (hue [0..360], saturation [0..1], luma [0..1]) to RGB Hex (#xxxxxx)
+
+    const a = s * Math.min(l, 1 - l);
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+function hexToHsl(color) {
+    // Convert RGB Hex (#xxxxxx) to HSL (hue [0..360], saturation [0..1], luma [0..1])
+
+    var r = parseInt(color.substr(1, 2), 16) / 255; 
+    var g = parseInt(color.substr(3, 2), 16) / 255;
+    var b = parseInt(color.substr(5, 2), 16) / 255;
+
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h * 360, s, l];
+}
+
 // Functions
 
 function changeAtemNextTransitionState() {
@@ -51,6 +91,16 @@ $("#atem-key1-on-air-next-transition-btn").click((e) => {
 
 $("#atem-capture-still-btn").click((e) => {
     atem.post("capture-still");
+})
+
+$("#atem-color1-input").change((e) => {
+    let color = hexToHsl($("#atem-color1-input").val());
+    atem.post('color-generator', { index: 0, hue: color[0], saturation: color[1], luma: color[2] });
+})
+
+$("#atem-color2-input").change((e) => {
+    let color = hexToHsl($("#atem-color2-input").val());
+    atem.post('color-generator', { index: 1, hue: color[0], saturation: color[1], luma: color[2] });
 })
 
 // Events
@@ -107,6 +157,13 @@ $(window).on("atem-get-transition-position", function (e, data) {
     $("#atem-transition-auto-btn").toggleClass("btn-danger", active).toggleClass("pulsing", active).toggleClass("btn-outline-primary", !active);
 });
 
+$(window).on("atem-get-color-generator", function (e, data) {
+    let col1 = hslToHex(data["0"].hue, data["0"].saturation, data["0"].luma);
+    let col2 = hslToHex(data["1"].hue, data["1"].saturation, data["1"].luma);
+    $("#atem-color1-input").val(col1);
+    $("#atem-color2-input").val(col2);
+});
+
 // Interval
 
 $(window).on("atem-base-interval", function () {
@@ -116,4 +173,5 @@ $(window).on("atem-base-interval", function () {
     atem.get("transition-settings");
     atem.get("transition-position");
     atem.get("mediaplayer-file-info");
+    atem.get("color-generator");
 });
