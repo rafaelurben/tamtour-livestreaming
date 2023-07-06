668874863911c4530b4336edf46eda60bacfcf4e
rafaelurben <github@rafaelurben.ch>
2023-07-06 02:11:57 +0200

    Added basic drag and drop reordering of widgets

--- controlpanel/static/js/general.js
+++ controlpanel/static/js/general.js
@@ -19,3 +19,42 @@ async function resetSettings() {
 function toggleCollapse(elem) {
     $(elem).parent().parent().toggleClass("collapsed");
 }
+
+// Drag and drop
+
+$(document).ready(function () {
+    var $dragging = null;
+
+    $draggers = $("fieldset > legend > span.collapser");
+    $draggables = $("fieldset");
+    $dragtargets = $(".dragtarget");
+
+    $draggers.on('mouseenter', function () { $(this).parent().parent().attr("draggable", true) });
+    $draggers.on('mouseleave', function () { $(this).parent().parent().attr("draggable", false) }); 
+
+    $draggables.on('dragstart', function (e) {
+        $dragging = $(this);
+        $(".dragtarget").addClass("droptarget-active");
+    });
+    $draggables.on('dragend', function (e) {
+        $dragging = null;
+        $('.dragover').removeClass("dragover");
+        $(".dragtarget").removeClass("droptarget-active");
+    });
+
+    $dragtargets.on('dragover', function (e) {
+        e.preventDefault();
+        $(this).addClass("dragover");
+    });
+    $dragtargets.on('dragleave', function (e) {
+        e.preventDefault();
+        $('.dragover').removeClass("dragover");
+    });
+    $dragtargets.on('drop', function (e) {
+        e.preventDefault();
+        $('.dragover').removeClass("dragover");
+        $(".dragtarget").removeClass("droptarget-active");
+        
+        $dragging.insertBefore($(this));
+    });
+});
