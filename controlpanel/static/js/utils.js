61e9c120bafd107313e204031c189956fafd2890
rafaelurben <github@rafaelurben.ch>
2023-07-09 01:40:33 +0200

    Moved color conversion into seperate module

---
+++ controlpanel/static/js/utils.js
@@ -0,0 +1,39 @@
+// Color Conversion
+
+function hslToHex(h, s, l) {
+    // Convert HSL (hue [0..360], saturation [0..1], luma [0..1]) to RGB Hex (#xxxxxx)
+
+    const a = s * Math.min(l, 1 - l);
+    const f = n => {
+        const k = (n + h / 30) % 12;
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+        return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
+
+function hexToHsl(color) {
+    // Convert RGB Hex (#xxxxxx) to HSL (hue [0..360], saturation [0..1], luma [0..1])
+
+    var r = parseInt(color.substr(1, 2), 16) / 255;
+    var g = parseInt(color.substr(3, 2), 16) / 255;
+    var b = parseInt(color.substr(5, 2), 16) / 255;
+
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if (max == min) {
+        h = s = 0; // achromatic
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h * 360, s, l];
+}
