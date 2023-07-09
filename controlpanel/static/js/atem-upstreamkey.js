9938de6bb9123d8193b859806f1d5ff67a589dfc
rafaelurben <github@rafaelurben.ch>
2023-07-09 02:01:02 +0200

    ATEM: Added apply button and changed data structure

--- controlpanel/static/js/atem-upstreamkey.js
+++ controlpanel/static/js/atem-upstreamkey.js
@@ -20,84 +20,103 @@ atemKEY1TypeselectElem.change(function (e) {
 
 // DVE
 
-function updateKey1DVEForm() {
-    let dat = atem.state.key1DVEdata;
+function updateKey1DVEFormFromData() {
+    let local_data = atem.state.key1DVEdata;
 
-    let anchors = $("#key1-dve-form-anchor").val().split("-");
-    let anchory = anchors[0];
-    let anchorx = anchors[1];
+    let anchors = $("#atem-key1-dve-form-anchor").val().split("-");
+    let anchor_x = anchors[0];
+    let anchor_y = anchors[1];
 
-    var pos_x = dat.pos_x / 32000 + 0.5;
-    var pos_y = dat.pos_y / 18000 + 0.5;
-    var size_x = dat.size_x / 1000;
-    var size_y = dat.size_y / 1000;
+    var pos_x = local_data.pos_x;
+    var pos_y = local_data.pos_y;
+    var size_x = local_data.size_x;
+    var size_y = local_data.size_y;
 
-    console.log(pos_x, pos_y, size_x, size_y, anchorx, anchory)
-
-    if (anchorx == "left") {
+    if (anchor_x == "left") {
         pos_x -= size_x / 2;
-    } else if (anchorx == "right") {
+    } else if (anchor_x == "right") {
         pos_x += size_x / 2;
     }
 
-    if (anchory == "bottom") {
+    if (anchor_y == "bottom") {
         pos_y -= size_y / 2;
-    } else if (anchory == "top") {
+    } else if (anchor_y == "top") {
         pos_y += size_y / 2;
     }
 
-    $("#key1-dve-form-posx").val(pos_x);
-    $("#key1-dve-form-posy").val(pos_y);
-    $("#key1-dve-form-sizex").val(size_x);
-    $("#key1-dve-form-sizey").val(size_y);
+    $("#atem-key1-dve-form-posx").val(pos_x);
+    $("#atem-key1-dve-form-posy").val(pos_y);
+    $("#atem-key1-dve-form-sizex").val(size_x);
+    $("#atem-key1-dve-form-sizey").val(size_y);
+}
+
+function updateKey1DVEDataFromForm() {
+    var pos_x = parseFloat($("#atem-key1-dve-form-posx").val());
+    var pos_y = parseFloat($("#atem-key1-dve-form-posy").val());
+    var size_x = parseFloat($("#atem-key1-dve-form-sizex").val());
+    var size_y = parseFloat($("#atem-key1-dve-form-sizey").val());
+
+    let anchors = $("#atem-key1-dve-form-anchor").val().split("-");
+    let anchor_x = anchors[0];
+    let anchor_y = anchors[1];
+
+    if (anchor_x == "left") {
+        pos_x += size_x / 2;
+    } else if (anchor_x == "right") {
+        pos_x -= size_x / 2;
+    }
+
+    if (anchor_y == "bottom") {
+        pos_y += size_y / 2;
+    } else if (anchor_y == "top") {
+        pos_y -= size_y / 2;
+    }
+
+    atem.state.key1DVEdata = {
+        pos_x: pos_x,
+        pos_y: pos_y,
+        size_x: size_x,
+        size_y: size_y,
+    }
 }
 
 $("#atem-key1-dve-dialog").on("show.bs.modal", function (e) {
     atem.get("key-properties-dve");
 });
 
-$("#key1-dve-form-anchor").change(function (e) {
-    updateKey1DVEForm();
+$("#atem-key1-dve-form-anchor").change(function (e) {
+    updateKey1DVEFormFromData();
+});
+
+
+$("#atem-key1-dve-dialog input").change(function (e) {
+    updateKey1DVEDataFromForm();
 });
 
 $(window).on("atem-get-key-properties-dve", function (e, data) {
-    atem.state.key1DVEdata = data["0"]["0"];
-    updateKey1DVEForm();
+    let atem_data = data["0"]["0"];
+
+    atem.state.key1DVEdata = {
+        pos_x: atem_data.pos_x / 32000 + 0.5,
+        pos_y: atem_data.pos_y / 18000 + 0.5,
+        size_x: atem_data.size_x / 1000,
+        size_y: atem_data.size_y / 1000,
+    }
+
+    updateKey1DVEFormFromData();
 });
 
-$("#atem-key1-dve-dialog input").change(function (e) {
-    var pos_x = parseFloat($("#key1-dve-form-posx").val());
-    var pos_y = parseFloat($("#key1-dve-form-posy").val());
-    var size_x = parseFloat($("#key1-dve-form-sizex").val());
-    var size_y = parseFloat($("#key1-dve-form-sizey").val());
+$("#atem-key1-dve-form-apply").click(function (e) {
+    let local_data = atem.state.key1DVEdata;
 
-    let anchors = $("#key1-dve-form-anchor").val().split("-");
-    let anchory = anchors[0];
-    let anchorx = anchors[1];
-
-    if (anchorx == "left") {
-        pos_x += size_x / 2;
-    } else if (anchorx == "right") {
-        pos_x -= size_x / 2;
-    }
-
-    if (anchory == "bottom") {
-        pos_y += size_y / 2;
-    } else if (anchory == "top") {
-        pos_y -= size_y / 2;
-    }
-
-    data = {
+    atem.post("key-properties-dve", {
         index: 0,
         keyer: 0,
-        pos_x: parseInt((pos_x - 0.5) * 32000),
-        pos_y: parseInt((pos_y - 0.5) * 18000),
-        size_x: parseInt(size_x * 1000),
-        size_y: parseInt(size_y * 1000),
-    }
-
-    atem.post("key-properties-dve", data)
-    atem.state.key1DVEdata = data;
+        pos_x: parseInt((local_data.pos_x - 0.5) * 32000),
+        pos_y: parseInt((local_data.pos_y - 0.5) * 18000),
+        size_x: parseInt(local_data.size_x * 1000),
+        size_y: parseInt(local_data.size_y * 1000),
+    })
 });
 
 // SET / RUN
