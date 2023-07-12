e1a6665c1db8ce02fffe85b14912aeba8abd242d
rafaelurben <github@rafaelurben.ch>
2023-07-12 18:33:46 +0200

    Control: Fixed drag&drop issues

--- controlpanel/static/js/general.js
+++ controlpanel/static/js/general.js
@@ -10,15 +10,21 @@ $(document).ready(function () {
     var $dragging = null;
 
     $draggers = $("fieldset.droptarget > legend > span.collapser");
-    $draggables = $("fieldset");
+    $draggables = $("fieldset.droptarget");
     $droptargets = $(".droptarget");
 
     $draggers.on('mouseenter', function () { $(this).parent().parent().attr("draggable", true) });
     $draggers.on('mouseleave', function () { $(this).parent().parent().attr("draggable", false) }); 
 
     $draggables.on('dragstart', function (e) {
+        if (e.target.tagName !== "FIELDSET") return;
+
         $dragging = $(this);
         $(".droptarget").addClass("droptarget-active");
+
+        // Hide nonsense targets (e.g. the one we're dragging)
+        $dragging.removeClass("droptarget-active");
+        $dragging.next("fieldset.droptarget").removeClass("droptarget-active");
     });
     $draggables.on('dragend', function (e) {
         $dragging = null;
