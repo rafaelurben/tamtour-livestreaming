8a8b86d3920bed7424fcbde6838963f0311f23d5
rafaelurben <github@rafaelurben.ch>
2025-02-26 23:51:31 +0100

    fix(control-panel): Prevent unnecessary iframe errors for YouTube Live chat embeds.

--- controlpanel/static/js/youtube.js
+++ controlpanel/static/js/youtube.js
@@ -2,10 +2,20 @@ let $ytLiveChatEmbed = $("#youtube-livechat-embed");
 
 function askAndSetYTLiveID() {
     let ytId = prompt("Enter YouTube Video ID:");
-    if (ytId) setYTLiveID(ytId);
+    setYTLiveID(ytId);
 }
 
 function setYTLiveID(videoId) {
-    let url = `https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${location.host}`;
+    if (location.protocol === 'file:') {
+        alert("YouTube-Embeds funktionieren nicht in file:/// URLs.")
+        return;
+    }
+
+    let url;
+    if (videoId) {
+        url = `https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${location.host}`;
+    } else {
+        url = 'about:blank';
+    }
     $ytLiveChatEmbed.attr("src", url);
 }
