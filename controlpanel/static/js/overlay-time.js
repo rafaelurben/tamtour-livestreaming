23166fd42e6b317bac1855b05f560c27407476e1
rafaelurben <github@rafaelurben.ch>
2025-02-27 00:10:50 +0100

    feat(control-panel): Show current time in time overlay panel

--- controlpanel/static/js/overlay-time.js
+++ controlpanel/static/js/overlay-time.js
@@ -1,5 +1,12 @@
 /* Time overlay */
 
+function _updateTimeOverlayTime() {
+    let time = new Date();
+    let hours = String(time.getHours()).padStart(2, "0");
+    let minutes = String(time.getMinutes()).padStart(2, "0");
+    let seconds = String(time.getSeconds()).padStart(2, "0");
+    $("#time-display").val(`${hours}:${minutes}:${seconds}`);
+}
 
 async function showTimeOverlay() {
     let success = await obs.sendAction("showTimeOverlay");
@@ -7,7 +14,9 @@ async function showTimeOverlay() {
 
     let button = $("#btn-show-time-overlay");
     button.prop("disabled", true);
-    setTimeout(() => { button.prop("disabled", false) }, 1000);
+    setTimeout(() => {
+        button.prop("disabled", false)
+    }, 1000);
 
     return true;
 }
@@ -18,7 +27,13 @@ async function hideTimeOverlay() {
 
     let button = $("#btn-hide-time-overlay");
     button.prop("disabled", true);
-    setTimeout(() => { button.prop("disabled", false) }, 1000);
+    setTimeout(() => {
+        button.prop("disabled", false)
+    }, 1000);
 
     return true;
 }
+
+window.addEventListener('load', function () {
+    setInterval(_updateTimeOverlayTime, 1000)
+});
